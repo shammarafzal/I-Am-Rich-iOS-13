a82bd7a66e85b1112f5ca16182ae3c15ebc57333
Ammar Afzal <ammarafzal075@gmail.com>
2020-09-19 09:54:24 +0500

    Add files via upload

---
+++ I Am Rich ios 13/I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Ammar Afzal on 09/09/2020.
+//  Copyright © 2020 Ammar Afzal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
